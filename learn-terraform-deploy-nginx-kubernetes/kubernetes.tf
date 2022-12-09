0d1b125c40c2544378e3b1ec1d306af4a7f45a86
Rick Jury <rjury@sumologic.com>
2022-12-09 15:58:19 +1300

    extenal step

--- learn-terraform-deploy-nginx-kubernetes/kubernetes.tf
+++ learn-terraform-deploy-nginx-kubernetes/kubernetes.tf
@@ -91,3 +91,25 @@ resource "kubernetes_deployment" "nginx" {
     }
   }
 }
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-example"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
+output "lb_ip" {
+  value = kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.hostname
+}
+
