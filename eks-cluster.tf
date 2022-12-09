26be2a99a4d0b4bf3f0630c868b82bd5841ef377
Rick Jury <rjury@sumologic.com>
2022-12-09 15:58:35 +1300

    required or k8s collection wont schedule

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -21,11 +21,12 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.small"]
+      # instance_types = ["t3.small"] 
+      instance_types = ["t3.large"]
 
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      max_size     = 4
+      desired_size = 3
 
       pre_bootstrap_user_data = <<-EOT
       echo 'foo bar'
